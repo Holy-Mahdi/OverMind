d25eefa95b58b9bb3a461cc9a7a019b40bf12216
Mahdi Mohammadi <mohammadimahdiofficial@gmail.com>
2025-06-25 13:48:14 +0330

    Add initial game setup with Pygame

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("OverMind - a RTS game")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((0, 0, 0))  
+    pygame.display.flip()
+
+pygame.quit()
+exit(0)
